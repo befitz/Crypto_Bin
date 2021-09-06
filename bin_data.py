aa297d60661c1ed810c7d0472b6298bebd3df969
Brynne Fitzgerald <brynnefitzgerald@gmail.com>
2021-09-06 13:46:10 -0400

    make ticker a variable

--- bin_data.py
+++ bin_data.py
@@ -5,11 +5,14 @@ from binance.client import Client
 from binance import AsyncClient, BinanceSocketManager
 import config
 
+#Set this as the crypto to get prices for
+crypto_ticker = 'SOLUSD'
+
 #Establish connection to Binance via API key
 client = Client(config.apiKey, config.apiSecurity, tld='us')
 print("Logged in!")
 #Set up a SQL database to store timestamps and prices
-engine = sqlalchemy.create_engine('sqlite:///ADAUSDstream.db')
+engine = sqlalchemy.create_engine(f'sqlite:///{crypto_ticker}stream.db')
 
 #Creates a dataframe of the timestamps and prices by second
 def createframe(msg):
@@ -26,11 +29,11 @@ async def main():
 	while True:
 		client = await AsyncClient.create(tld='us')
 		bm = BinanceSocketManager(client)
-		socket = bm.trade_socket('ADAUSD')
+		socket = bm.trade_socket(crypto_ticker)
 		await socket.__aenter__()
 		msg = await socket.recv()
 		frame = createframe(msg)
-		frame.to_sql('ADAUSD', engine, if_exists='append', index = False)
+		frame.to_sql(crypto_ticker, engine, if_exists='append', index = False)
 		print(frame)
 
 		await client.close_connection()
--- strategy_test.py
+++ strategy_test.py
@@ -7,19 +7,21 @@ import config
 client = Client(config.apiKey, config.apiSecurity, tld='us')
 print("Logged in!")
 
-engine = sqlalchemy.create_engine('sqlite:///ADAUSDstream.db')
+crypto_ticker = 'SOLUSD'
+#Set up a SQL database to fetch timestamps and prices
+engine = sqlalchemy.create_engine(f'sqlite:///{crypto_ticker}stream.db')
 
 #Simple trendfollowing strategy
 #If crypto (ADAUSD) is rising by x%, place a buy market order
 #Exit when the profit is above 0.15% or loss is less than -0.15%
 def strategy(entry, lookback, qty, open_position=False):
 	while True:
-		df = pd.read_sql('ADAUSD', engine)
+		df = pd.read_sql(crypto_ticker, engine)
 		lookbackperiod = df.iloc[-lookback:]
 		cumret = (lookbackperiod.Price.pct_change() +1).cumprod() -1
 		if not open_position:
 			if cumret[cumret.last_valid_index()] > entry:
-				order = client.create_order(symbol='ADAUSD',
+				order = client.create_order(symbol=crypto_ticker,
 					side = 'BUY',
 					type = 'MARKET',
 					quantity = qty)
@@ -29,17 +31,17 @@ def strategy(entry, lookback, qty, open_position=False):
 
 	if open_position:
 		while True:
-			df = pd.read_sql('ADAUSD', engine)
+			df = pd.read_sql(crypto_ticker, engine)
 			sincebuy = df.loc[df.Time > pd.to_datetime(order['transactTime'], unit = 'ms')]
 			if len(sincebuy) >1:
 				sincebuyret = (sincebuy.Price.pct_change() +1).cumprod() -1
 				last_entry = sincebuyret[sincebuyret.last_valid_index()]
 				if last_entry > 0.0015 or last_entry <0.0015:
-					order = client.create_order(symbol = 'ADAUSD',
+					order = client.create_order(symbol = crypto_ticker,
 						side = 'SELL', 
 						type = 'MARKET',
 						quantity = qty)
 					print(order)
 					break
 
-strategy(0.001, 60, 9)
+#strategy(0.001, 60, 9)
