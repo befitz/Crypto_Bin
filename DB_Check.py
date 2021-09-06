68cf8b9e2314b3aef6a9d0e836299fb54d774110
Brynne Fitzgerald <brynnefitzgerald@gmail.com>
2021-09-06 15:22:24 -0400

    Create DB_Check.py

---
+++ DB_Check.py
@@ -0,0 +1,18 @@
+import sqlalchemy
+import pandas as pd
+
+
+
+def check_database(lookback, crypto_ticker, USD_amount):
+	lookback = 60
+	crypto_ticker = crypto_ticker
+	engine = sqlalchemy.create_engine(f'sqlite:///{crypto_ticker}stream.db')
+	df = pd.read_sql(crypto_ticker, engine)
+	lookbackperiod = df.iloc[-lookback:]
+	latest_price = df.iloc[-1].Price
+	qty = USD_amount/latest_price
+	print("Quantity:",qty)
+	print("Price:", latest_price)
+
+
+check_database(60, 'ALGOUSD', 50)
