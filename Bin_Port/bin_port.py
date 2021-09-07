5b26df1bfa10b004a9280d87bf8932221b4c6bfe
Brynne Fitzgerald <brynnefitzgerald@gmail.com>
2021-09-06 23:00:06 -0400

    Update bin_port.py

--- Bin_Port/bin_port.py
+++ Bin_Port/bin_port.py
@@ -1,7 +1,7 @@
 import xlwings
 import pandas as pd 
 from binance.client import Client
-import config
+from .. import config
 
 client = Client(config.apiKey, config.apiSecurity, tld='us')
 print("Logged in, leggo!")
