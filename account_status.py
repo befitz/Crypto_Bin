6e4f36aa59248e438d52532493836c7063022d41
Brynne Fitzgerald <brynnefitzgerald@gmail.com>
2021-09-06 22:38:06 -0400

    Update account_status.py

--- account_status.py
+++ account_status.py
@@ -1,5 +1,7 @@
 from binance.client import Client
 import config
+import pandas as pd 
+
 
 #Establish connection to Binance via API key
 client = Client(config.apiKey, config.apiSecurity, tld='us')
@@ -8,4 +10,6 @@ print("Logged in, leggo!")
 crypto_ticker = config.crypto_ticker
 
 orders = client.get_open_orders(symbol=crypto_ticker)
+open_orders = pd.Dataframe([orders])
+
 print(orders)
