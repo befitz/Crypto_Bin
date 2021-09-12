47dfdb9618c329eaf28b9e0f6e4ebd641ab5346a
Brynne Fitzgerald <brynnefitzgerald@gmail.com>
2021-09-12 18:26:15 -0400

    Update test_indicators.py

--- tests/test_indicators.py
+++ tests/test_indicators.py
@@ -16,8 +16,8 @@ def test_macd_calc():
 	inputs: price_history is a dummy .csv file with made up Time, Close, and Volume
 	? Cannot import _macd_calc to test this??
 	"""
-	price_history = pd.read_csv('price_history_test.csv')
-	expected_results = pd.read_csv('price_history_macd_test.csv')
+	price_history = pd.read_csv('csv_test_files/price_history_test.csv')
+	expected_results = pd.read_csv('csv_test_files/price_history_macd_test.csv')
 	price_history_macd = _MACD_calc(price_history)
 	macd_calc_test_result = assert_frame_equal(expected_results, price_history_macd)
 
@@ -31,7 +31,7 @@ class TestIndicator(unittest.TestCase):
 		This function will test the retun of a 0 from macd_signal
 		The input is the 'price_history_test.csv' which has an increasing price for the past 49 intervals so expected result is a HOLD (0) trading signal
 		"""
-		price_history = pd.read_csv('price_history_test.csv')
+		price_history = pd.read_csv('csv_test_files/price_history_test.csv')
 		expected_result = 0 
 		latest_signal = indicators.macd_signal(price_history)
 		self.assertEqual(latest_signal, expected_result)
@@ -41,7 +41,7 @@ class TestIndicator(unittest.TestCase):
 		This function tests the return of a -1 or SELL from macd_signal
 		Input is 'price_history_testSELL.csv' which has the latest price decreased significantly.
 		"""
-		price_history = pd.read_csv('price_history_testSELL.csv')
+		price_history = pd.read_csv('csv_test_files/price_history_testSELL.csv')
 		expected_result = -1
 		latest_signal = indicators.macd_signal(price_history)
 		self.assertEqual(latest_signal, expected_result)
@@ -51,7 +51,7 @@ class TestIndicator(unittest.TestCase):
 		This function will test the return of a 1 or BUY from macd_signal
 		Input is 'price_history_testBUY.csv' which has the latest price increased.
 		"""
-		price_history = pd.read_csv('price_history_testBUY.csv')
+		price_history = pd.read_csv('csv_test_files/price_history_testBUY.csv')
 		expected_result = 1
 		latest_signal = indicators.macd_signal(price_history)
 		self.assertEqual(latest_signal, expected_result)
