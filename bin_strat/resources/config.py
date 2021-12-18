d2bf5ccae65eeef014adfffb7cdfb61c89c54f34
unknown <asoares1@chewy.com>
2021-12-18 01:13:42 -0500

    add some comments and shit idk

--- bin_strat/resources/config.py
+++ bin_strat/resources/config.py
@@ -3,18 +3,27 @@ import logging as log
 import yaml
 from os.path import exists
 
-log.root.setLevel(log.INFO)
 
 properties = {}
 
-_local_cfg_fp = './application-local.yml'
+
 _application_cfg_fp = './application.yml'
+"""
+Main Property File for running on the cloud. Contains environment variable references but no secret keys.
+File is not gitignored, and exists in the public repository.
+"""
+
+
+_local_cfg_fp = './application-local.yml'
+"""
+Local Property File for running the app locally. May contain environment variable references, passwords and secret keys. 
+File is gitignored, and safe to use locally.
+"""
+
 
 def interpolate_environment_dict(cfg):
     """
-    Returns a new dictionary where environment variable properties are loaded.
-    An environment variable property is any value that begins with '$'. (ex: $BINANCE_API_KEY)
-    Ignores any values that do not begin with '$', or are not a string.
+    Returns a new dict where all references to an environment variable are replaced with the actual value.
     Arguments:
         cfg (dict) the dictionary to interpolate.
     Returns:
@@ -31,6 +40,13 @@ def interpolate_environment_dict(cfg):
     return tree
 
 def interpolate_environment_list(cfg):
+    """
+    Returns a new list where all references to an environment variable are replaced with the actual value.
+    Arguments:
+        cfg (list) the list to interpolate.
+    Returns:
+        list: a list of indentical size where environment variables replace properties.
+    """
     tree = []
     for index in range(len(cfg)):
         value = cfg[index]
@@ -43,6 +59,20 @@ def interpolate_environment_list(cfg):
     return tree
 
 def interpolate_environment_value(cfg):
+    """
+    Given a key of any type, retrieve environment variable.
+    Key must be a string, and must begin with a '$' character.
+
+    Examples:
+        - cfg is '$X_MBX_APIKEY' -> replaces property with value of os.getenv("X_MBX_APIKEY")
+        - cfg is 'myapikeyvalue' -> not an environment variable reference, value is copied
+        - cfg is 0 -> not a string, value is copied
+    Args:
+        cfg (any): the value to retrieve or ignore
+
+    Returns:
+        any: the value either converted or ignored
+    """
     if isinstance(cfg, str) and cfg.startswith('$'):
         return os.getenv(cfg[1:], '')
     return cfg
@@ -50,6 +80,7 @@ def interpolate_environment_value(cfg):
 def set_property_export(cfg_fp):
     with open(cfg_fp) as property_file:
         properties_dict: dict = yaml.safe_load(property_file)['properties']
+        # Assume YAML files always begin as dictionaries
         return interpolate_environment_dict(properties_dict)
 
 if __name__ == '__main__':
