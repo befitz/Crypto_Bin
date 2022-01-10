0e26a8dee8ab3c3a0ff8b9f5b06487305eee7a11
unknown <asoares1@chewy.com>
2022-01-10 01:19:31 -0500

    add names to test cases

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,7 @@
 import io
 import os
 import unittest
+import logging as log
 from unittest.mock import patch
 
 from parameterized import parameterized
@@ -16,11 +17,13 @@ class ConfigTest(unittest.TestCase):
         self.assertRaises(FileNotFoundError, load_property_configurations)
 
     @parameterized.expand([
-        (False,
+        ("No Local Property File, No Environment Interpolation",
+         False,
          [io.StringIO('{properties: {run-interval: 2h, candlesticks: 80}}')],
          {'properties': {'run-interval': '2h', 'candlesticks': 80}}),
 
-        (True,
+        ("Local Property File Overrides List, Replaces Strings, Replaces Integers",
+         True,
          [io.StringIO('{xx: {yy: {zz: [xxx, yyy, zzz], ww: www, aa: 5}}}'),
           io.StringIO('{xx: {yy: {zz: [aaa, bbb, ccc], ww: abc, aa: 10}}}')],
          {
@@ -33,37 +36,50 @@ class ConfigTest(unittest.TestCase):
              }
          }),
 
-        (True,
+        ("Local Property File Does Not Modify Original",
+         True,
          [io.StringIO('{properties: {run-interval: 2h, candlesticks: 80}}'),
-          io.StringIO('{properties: {run-interval: 2h, candlesticks: 90}}')],
-         {'properties': {'run-interval': '2h', 'candlesticks': 90}})
+          io.StringIO('{properties: {}}')],
+         {'properties': {'run-interval': '2h', 'candlesticks': 80}}),
+
+        ("Local Property File Cannot Create New Properties",
+         True,
+         [io.StringIO('{properties: {run-interval: 2h, candlesticks: 80}}'),
+          io.StringIO('{properties: {binance-api-key: X-MBX-APIKEY}}')],
+         {'properties': {'run-interval': '2h', 'candlesticks': 80}})
     ])
     @patch('bin_strat.resources.config.open')
     @patch('bin_strat.resources.config.exists')
-    def test_yaml_loading(self, exists_local, open_input, expected, exists_mock, open_mock):
+    def test_yaml_loading(self, name, exists_local, open_input, expected, exists_mock, open_mock):
+        log.info("running test %s".format(name))
         exists_mock.side_effect = [True, exists_local]
         open_mock.side_effect = open_input
         self.assertEqual(load_property_configurations(), expected)
 
     @parameterized.expand([
-        ({'INTERVAL': '2h', 'CANDLESTICKS': '80'},
+        ("Loads Integer and String Value from Environment - Local Overrides Main",
+         {'INTERVAL': '2h', 'CANDLESTICKS': '80'},
          [io.StringIO('{properties: {run-interval: $INTERVAL, candlesticks: 200}}'),
           io.StringIO('{properties: {candlesticks: $CANDLESTICKS}}')],
          {'properties': {'run-interval': '2h', 'candlesticks': 80}}),
 
-        ({'INTERVAL': '2h', 'CANDLESTICKS': '80'},
+        ("Loads String and Integer Value from Environment - Local Overrides Main #2",
+         {'INTERVAL': '2h', 'CANDLESTICKS': '80'},
          [io.StringIO('{properties: {run-interval: 5h, candlesticks: 200}}'),
           io.StringIO('{properties: {run-interval: $INTERVAL, candlesticks: $CANDLESTICKS}}')],
          {'properties': {'run-interval': '2h', 'candlesticks': 80}}),
 
-        ({'INTERVAL': '2h', 'CANDLESTICKS': '80.2'},
+        ("Loads Float and String Value from Environment - No Local Properties",
+         {'INTERVAL': '2h', 'CANDLESTICKS': '80.2'},
          [io.StringIO('{properties: {run-interval: $INTERVAL, candlesticks: $CANDLESTICKS}}'),
           io.StringIO('{properties: {}}')],
          {'properties': {'run-interval': '2h', 'candlesticks': 80.2}}),
     ])
     @patch('bin_strat.resources.config.open')
     @patch('bin_strat.resources.config.exists')
-    def test_environment_interpolation(self, env, open_input, expected, exists_mock, open_mock):
+    def test_environment_interpolation(self, name, env, open_input, expected, exists_mock, open_mock):
+        log.info("running test %s".format(name))
+
         for e in env.keys():
             os.environ[e] = env[e]
 
